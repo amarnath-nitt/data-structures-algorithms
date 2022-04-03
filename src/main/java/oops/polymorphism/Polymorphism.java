8d328d1d29604141891259249f5712307c589193
amarnath-nitt <amarnath97.nitt@gmail.com>
2022-04-03 13:36:54 +0530

    putted signature in polymoriphism & it's implementation

---
+++ src/main/java/oops/polymorphism/Polymorphism.java
@@ -0,0 +1,12 @@
+package main.java.oops.polymorphism;
+/*
+ * @author amarnath-nitt on 03-04-2022;
+ */
+public class Polymorphism {
+    public static void main(String []args){
+        CashPayment cashPayment = new CashPayment();
+        cashPayment.pay();
+        CreditPayment creditPayment = new CreditPayment();
+        creditPayment.pay();
+    }
+}
